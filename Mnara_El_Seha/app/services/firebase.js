ec7240e7d7f67ed5c80e75460a941c3db2e40f0c
Mohamed Kamal <medoking91@gmail.com>
2023-08-01 12:14:11 +0300

    feat: auth

--- Mnara_El_Seha/app/services/firebase.js
+++ Mnara_El_Seha/app/services/firebase.js
@@ -1,30 +1,5 @@
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCMnDYCARsU4j9tWB6nd0cvlK_oIDIdtWI",
-  authDomain: "manara-el-seha.firebaseapp.com",
-  projectId: "manara-el-seha",
-  storageBucket: "manara-el-seha.appspot.com",
-  messagingSenderId: "845463856292",
-  appId: "1:845463856292:web:8797e0acb81e1227f1b231",
-  measurementId: "G-QJWBLRMTD4"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-*/
-import * as firebase from 'firebase';
-// import { initializeApp } from 'firebase/app';
-
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -45,9 +20,5 @@ const firebaseConfig = {
   measurementId: MEASUREMENT_ID,
 };
 
-if (!firebase.apps.length) {
-  const app = firebase.initializeApp(firebaseConfig);
-  console.log(app);
-}
-
-export default firebase;
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
